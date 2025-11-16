6c48d2541730c32949b1df7933c705a0325a9c44
Trần Ngọc Tiến <trantiennt2019@gmail.com>
2025-11-17 01:56:15 +0700

    feat: update cart checkout flow without PayPal integration

--- src/api/cartAPI.js
+++ src/api/cartAPI.js
@@ -1,29 +1,38 @@
-
 import axiosInstance from "../config/axiosConfig/axios.config";
 import { API } from "../constants/hostAPI/hostAPI";
-// cal API
+
+/**
+ * Lấy tất cả giỏ hàng
+ */
 export const fetchCartsApi = async () => {
-    try {
-        const response = await axiosInstance.get(API.CARTS);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching CARTS data:", error);
-        throw error;
-    }
-};
-
-export const fetchCartByIDApi = async (id) => {
-    try {
-        const response = await axiosInstance.get(`${API.CARTS}/${id}`);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching Products data:", error);
-        throw error;
-    }
-};
-
-
-export const checkOutCartAPI = async (cartID) => {
-    const response = await axiosInstance.post(`${API.CARTS}/${cartID}/checkout`);
+  try {
+    const response = await axiosInstance.get(API.CARTS);
     return response.data;
+  } catch (error) {
+    console.error("Error fetching CARTS data:", error);
+    throw error;
+  }
 };
+
+/**
+ * Lấy giỏ hàng theo ID user
+ */
+export const fetchCartByIDApi = async (id) => {
+  try {
+    const response = await axiosInstance.get(`${API.CARTS}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching Products data:", error);
+    throw error;
+  }
+};
+
+/**
+ * CHECKOUT: gọi backend trừ kho + xoá item trong giỏ
+ * KHÔNG GỌI PAYPAL NỮA
+ */
+export const checkOutCartAPI = async (cartID) => {
+  const response = await axiosInstance.post(`${API.CARTS}/${cartID}/checkout`);
+  return response.data;
+};
+
